225d125c01f93f0b727c755dce423d9b31be0ba2
rastapanda <me@jspenc.com>
2015-10-13 22:13:32 +0100

    gulp config

---
+++ OpenGLExperiments/gulpfile.js
@@ -0,0 +1,21 @@
+﻿/// <binding BeforeBuild='before-build' />
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+gulp.task("sass-compile", function () {
+    gulp.src("./www/css/index.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./css/"));
+});
+
+gulp.task("watch-sass", function () {
+    console.log("watch sass");
+    gulp.watch("./www/css/*.scss", ["sass-compile"]);
+});
+
+
+gulp.task("before-build", ["sass-compile"], function () {
+    console.log("before build");
+
+    // Add anything you want to do before the build here
+});
